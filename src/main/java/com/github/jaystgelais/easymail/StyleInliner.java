e7c25b2fa5a567e592c8b7e6bacd50102785f3f7
Jay St.Gelais <JayStGelais@gmail.com>
2014-01-23 22:02:56 -0500

    Added stubbed StyleInliner utility.

---
+++ src/main/java/com/github/jaystgelais/easymail/StyleInliner.java
@@ -0,0 +1,21 @@
+package com.github.jaystgelais.easymail;
+
+/**
+ * Created by jaystgelais on 1/23/14.
+ */
+public final class StyleInliner {
+    private StyleInliner() {
+
+    }
+
+    /**
+     * Calculates teh effective styles of each HTML element and produces HTML output stripped of CSS classes and
+     * effective style inlined as 'style' attributes.
+     *
+     * @param contentProvider Input to style inlining transformation.
+     * @return HTML as String as product of style inlining transformation.
+     */
+    public static String inlineStyle(final HtmlContentProvider contentProvider) {
+        return null;
+    }
+}
