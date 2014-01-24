3a8a57363e6a20df515a7ae15a44b22e2de25da3
Jay St.Gelais <JayStGelais@gmail.com>
2014-01-24 11:32:05 -0500

    Added HTML Assertion utility to write better style inliner tests.

---
+++ src/test/java/com/github/jaystgelais/easymail/HtmlAssert.java
@@ -0,0 +1,78 @@
+package com.github.jaystgelais.easymail;
+
+import org.fit.cssbox.io.DOMSource;
+import org.fit.cssbox.io.DefaultDOMSource;
+import org.fit.cssbox.io.DocumentSource;
+import org.fit.cssbox.io.StreamDocumentSource;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Utility class that contains assertion methods for inspecting teh content of HTML documents.
+ */
+public final class HtmlAssert {
+    private HtmlAssert() {
+
+    }
+
+    public static void assertTagNotPresent(final String html, final String tag) throws IOException, SAXException {
+        assertFalse(isTagPresent(html, tag));
+    }
+
+    public static void assertTagPresent(final String html, final String tag) throws IOException, SAXException {
+        assertTrue(isTagPresent(html, tag));
+    }
+
+    public static void assertElementHasStyle(final String html, final String elementId, final String style)
+            throws IOException, SAXException, XPathExpressionException {
+        assertTrue(isStylePresentOnElement(html, elementId, style));
+    }
+
+    public static void assertElementDoesNotHaveStyle(final String html, final String elementId, final String style)
+            throws IOException, SAXException, XPathExpressionException {
+        assertFalse(isStylePresentOnElement(html, elementId, style));
+    }
+
+    public static boolean isStylePresentOnElement(final String html, final String elementId, final String style)
+            throws IOException, SAXException, XPathExpressionException {
+        Document doc = parseHtml(html);
+        Element element =  getElementById(doc, elementId);
+        if (element == null) {
+            return false;
+        }
+        return element.getAttribute("style").replaceAll("\\s+", "").toUpperCase()
+                .contains(style.replaceAll("\\s+", "").toUpperCase());
+    }
+
+    private static boolean isTagPresent(final String html, final String tag) throws IOException, SAXException {
+        Document doc = parseHtml(html);
+        return doc.getElementsByTagName(tag).getLength() > 0;
+    }
+
+    private static Document parseHtml(final String html) throws IOException, SAXException {
+        DocumentSource docSource = new StreamDocumentSource(
+                new ByteArrayInputStream(html.getBytes()), null, "text/html");
+        DOMSource parser = new DefaultDOMSource(docSource);
+        return parser.parse();
+    }
+
+    private static Element getElementById(final Document doc, final String id) throws XPathExpressionException {
+        String path = String.format("//*[@id = '%1$s' or @Id = '%1$s' or @ID = '%1$s' or @iD = '%1$s' ]", id);
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        NodeList nodes = (NodeList) xPath.evaluate(path, doc, XPathConstants.NODESET);
+
+        return (Element) nodes.item(0);
+    }
+
+}
--- src/test/java/com/github/jaystgelais/easymail/StyleInlinerTest.java
+++ src/test/java/com/github/jaystgelais/easymail/StyleInlinerTest.java
@@ -2,19 +2,41 @@ package com.github.jaystgelais.easymail;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.*;
-
 /**
  * Created by jaystgelais on 1/24/14.
  */
 public final class StyleInlinerTest {
-    private static final String TEST_HTML
-            = "<html><head><title>Hello World!</title></head><body>Hello World!</body></html>";
+    private static final String TEST_HTML_STYLE_APPLIED_TO_DIV_TAGS
+            = "<html><head><title>Sample Input</title><style>div {font-weight: bold;}</style></head>"
+            + "<body><div id=\"thediv\"></div></body></html>";
+
+    private static final String TEST_HTML_STYLE_APPLIED_TO_ID
+            = "<html><head><title>Sample Input</title><style>#thediv {font-weight: bold;}</style></head>"
+            + "<body><div id=\"thediv\"></div></body></html>";
+
+
+    private static final String TEST_HTML_STYLE_APPLIED_TO_CLASS
+            = "<html><head><title>Sample Input</title><style>.divStyle {font-weight: bold;}</style></head>"
+            + "<body><div id=\"thediv\" class=\"divStyle\"></div></body></html>";
 
     @Test
-    public void testInlineStyle() throws HtmlTransformationException {
-        HtmlContentProvider contentProvider = new StaticHtmlContentProvider(TEST_HTML);
+    public void testStylesAppliesToElementByType() throws Exception {
+        HtmlContentProvider contentProvider = new StaticHtmlContentProvider(TEST_HTML_STYLE_APPLIED_TO_DIV_TAGS);
         String htmlOutout = StyleInliner.inlineStyle(contentProvider);
-        assertEquals(TEST_HTML, htmlOutout);
+        HtmlAssert.assertElementHasStyle(htmlOutout, "thediv", "font-weight: bold;");
+    }
+
+    @Test
+    public void testStylesAppliesToElementById() throws Exception {
+        HtmlContentProvider contentProvider = new StaticHtmlContentProvider(TEST_HTML_STYLE_APPLIED_TO_ID);
+        String htmlOutout = StyleInliner.inlineStyle(contentProvider);
+        HtmlAssert.assertElementHasStyle(htmlOutout, "thediv", "font-weight: bold;");
+    }
+
+    @Test
+    public void testStylesAppliesToElementByClass() throws Exception {
+        HtmlContentProvider contentProvider = new StaticHtmlContentProvider(TEST_HTML_STYLE_APPLIED_TO_CLASS);
+        String htmlOutout = StyleInliner.inlineStyle(contentProvider);
+        HtmlAssert.assertElementHasStyle(htmlOutout, "thediv", "font-weight: bold;");
     }
 }
